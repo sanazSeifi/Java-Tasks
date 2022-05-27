ba760b723443c736811f60dfb346a0bc7d102a41
Sanaz <snz5seifi@gmail.com>
2022-05-27 13:32:52 -0400

    Initialize Width, height, area and Parameter

--- src/main/java/com/example/getmehired_tasks/Assignment3.java
+++ src/main/java/com/example/getmehired_tasks/Assignment3.java
@@ -1,4 +1,39 @@
 package com.example.getmehired_tasks;
 
-public class Assignment3 {
+ public abstract class Assignment3 {
+     double hight;
+     double width;
+     double area;
+     double primeter;
+
+    // Hight
+     public void setHeight(double ht){
+         if (ht>hight)
+             ht = hight;
+         else System.out.println("Invalid hight: " + ht);
+     }
+     public double getHeight(){
+         return hight;
+     }
+
+
+     //width
+     public void  setWidth(double wt){
+         if(wt>width)
+             wt = width;
+         else System.out.println("Invalid Width: " + wt);
+     }
+     public double getWidth(){
+         return width;
+     }
+
+     //area
+     public void setArea(double ar){
+        ar = area;
+     }
+     public  double getArea(){
+         return area;
+     }
+
+
 }
