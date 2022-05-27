6dbfc836e3d28047c172b3934af7f467831daafb
Sanaz <snz5seifi@gmail.com>
2022-05-27 18:34:48 -0400

    Adding Triangle Class named RtTriangle

--- src/main/java/Assignment3/Rectangle.java
+++ src/main/java/Assignment3/Rectangle.java
@@ -1,4 +1,20 @@
 package Assignment3;
 
-public class Rectangle {
+public class Rectangle extends ShapClass{
+    double height;
+    double width;
+
+    public Rectangle(int numSide) {
+        super(numSide);
+    }
+
+    @Override
+    public double getArea() {
+        return height * width;
+    }
+
+    @Override
+    public double getPerimeter() {
+        return (height + width) * 2;
+    }
 }
