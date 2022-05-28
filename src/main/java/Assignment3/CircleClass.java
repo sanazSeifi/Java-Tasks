a7c772b9b5c4f0db6c992303b665ed52bd8748ae
Sanaz <snz5seifi@gmail.com>
2022-05-28 12:05:52 -0400

    Adding Area & Perimeter of Circle

--- src/main/java/Assignment3/CircleClass.java
+++ src/main/java/Assignment3/CircleClass.java
@@ -1,4 +1,19 @@
 package Assignment3;
 
-public class CircleClass {
+public class CircleClass extends ShapClass{
+    double circleRadian;
+    public CircleClass(double circleRadian) {
+        super(0);
+        this.circleRadian = circleRadian;
+    }
+
+    @Override
+    public double getArea() {
+        return circleRadian * circleRadian * 3.14;
+    }
+
+    @Override
+    public double getPerimeter() {
+        return circleRadian * 3.14;
+    }
 }
