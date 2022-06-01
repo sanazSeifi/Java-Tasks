a29f58a280017a7d9151b56cff139f5f26f559fa
Sanaz <snz5seifi@gmail.com>
2022-06-01 13:42:54 -0400

    Completeing MainClass and add related constructors to linkoln and ferrari and Jaguar

--- src/main/java/Assignment4/MainClass.java
+++ src/main/java/Assignment4/MainClass.java
@@ -2,17 +2,34 @@ package Assignment4;
 
 public class MainClass {
     public static void main(String[] args){
-        CarClass ferrari = new CarClass(4,"Ferrari",4);
+        CarClass ferrari = new Ferrari();
+        CarClass Ferrari2 = new Ferrari(4,ferrari,4, 5);
         System.out.println("I am ferrari");
         ferrari.setEngine("engine");
         ferrari.startEngine();
         ferrari.oilChange();
-        CarClass jaguar = new CarClass("Jaguar",4,4,100);
-        System.out.println("I am Jaguar");
-        jaguar.setEngine("engine");
-        jaguar.brake(3);
-        CarClass linkoln = new CarClass();
+        Ferrari ferrari3 = new Ferrari();
+        System.out.println(ferrari3.getName());
+        Ferrari2.brake();
+
+        CarClass jaguar = new Jaguar();
+        jaguar.brake();
+        Jaguar jaguar1 = new Jaguar();
+        jaguar1.accelerate();
+
+        CarClass linkoln = new Linkoln();
+        linkoln.brake();
+        linkoln.oilChange();
+        Linkoln linkoln1 = new Linkoln(linkoln,4,45,3);
+        linkoln1.accelerate(200);
+        System.out.println(linkoln1.getName());
+
+
+
+
+
 
     }
+
 }
 
