61a97510a1630f66c206d6a841f7ced0acbe8abf
Sanaz <snz5seifi@gmail.com>
2022-06-03 09:41:03 -0400

    Reverse Tree-Map

--- src/main/java/Assignment6/Reverse_Tree_Map.java
+++ src/main/java/Assignment6/Reverse_Tree_Map.java
@@ -1,4 +1,16 @@
 package Assignment6;
 
+import java.util.TreeMap;
+
 public class Reverse_Tree_Map {
+    public static void main(String[] args){
+        TreeMap treeMap = new TreeMap<>();
+        treeMap.put(1, "Jack");
+        treeMap.put(2, "Rick");
+        treeMap.put(3, "Kate");
+        treeMap.put(4, "Tom");
+        treeMap.put(5, "Steve");
+
+        System.out.println(" Output: " + treeMap + "\n");
+    }
 }
