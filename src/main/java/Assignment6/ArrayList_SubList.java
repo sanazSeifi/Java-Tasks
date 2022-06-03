484df2b5031ce4e2feb9e0dc289bd332a1b2d333
Sanaz <snz5seifi@gmail.com>
2022-06-03 07:37:35 -0400

    Add ArrayList with add Method

--- src/main/java/Assignment6/ArrayList_SubList.java
+++ src/main/java/Assignment6/ArrayList_SubList.java
@@ -1,4 +1,19 @@
 package Assignment6;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ArrayList_SubList {
+    public static void main(String[] args){
+        ArrayList arrayList = new ArrayList();
+        arrayList.add("Steve");
+        arrayList.add("Justin");
+        arrayList.add("Ajeet");
+        arrayList.add("John");
+        arrayList.add("Arnold");
+        arrayList.add("Chaitanya");
+        System.out.println("ArrayList is: " + arrayList);
+
+
+    }
 }
