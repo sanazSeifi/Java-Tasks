24a261181c5f5254814f6da3be103d6aaf0a770f
Sanaz <snz5seifi@gmail.com>
2022-06-08 12:37:20 -0400

    What is Thread and difference with process

--- src/main/java/Assignment8/Multi_Threading.java
+++ src/main/java/Assignment8/Multi_Threading.java
@@ -1,4 +1,18 @@
 package Assignment8;
 
 public class Multi_Threading {
+    /*
+    * 1- Thread is small portion of process and that's why we call that light-weight process. It is basic unit of CPU Utilization.
+    * Each program associate 1 or more processes and each process may execute 1 or more Threads(Traditional Thread and multiple Thread.
+    * different between Process and Thread:
+    * context switch is more heavy in process than Thread.
+    * process don't share memory but Thread can share memory with peer memory
+    * more time for communication between processes but let time between Threads
+    * If process locked the other processes can execute but if a Thread block the peer Threads will block as well.
+    * processes are independent but Threads are dependent of each other
+    * Processes get more time for creation and termination but threads less time.
+    * We use Thread because (Multi-Thread) to perform more than one task at a time.
+    *
+    * 2-
+    * */
 }
